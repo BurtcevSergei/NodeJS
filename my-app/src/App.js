0118bd8ad570fc9bbfb677cf667f5696adf42203
BurtcevSergei <bucciki@mail.ru>
2020-07-20 13:56:34 +0200

    1
    new files

---
+++ my-app/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Technologies from './Technologies.js';
+import Header from './Header.js';
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <Technologies />
+    </div>
+  );
+}
+
+export default App;
